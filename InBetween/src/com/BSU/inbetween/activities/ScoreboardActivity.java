d9501ecbbca0d626121611944a121275b8315b88
Brandon Groff <mynamesbg@gmail.com>
2015-01-19 01:21:53 -0500

    Began adding code and layout for a scoreboard that will show a player thei number of wins and losses. NOTE: Now using Android Studio instead of Eclipse.

---
+++ InBetween/src/com/BSU/inbetween/activities/ScoreboardActivity.java
@@ -0,0 +1,21 @@
+package com.BSU.inbetween.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by brandongroff on 1/19/15.
+ */
+public class ScoreboardActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        loadScoreboard();
+    }
+
+    public void loadScoreboard(){
+
+    }
+
+}
---
+++ InBetween/src/com/BSU/inbetween/activities/ScoreboardValues.java
@@ -0,0 +1,16 @@
+package com.BSU.inbetween.activities;
+
+import android.content.SharedPreferences;
+import android.content.res.Resources;
+
+/**
+ * Created by brandongroff on 1/19/15.
+ */
+public class ScoreboardValues {
+    private static SharedValues instance = null;
+    private Resources resources;
+    private SharedPreferences sharedValues;
+    private SharedPreferences.Editor editor;
+
+
+}
